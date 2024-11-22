1a60dfba2458f3a797a3c9f10fafad775ff12880
jcepriano <jamescepriano@gmail.com>
2024-11-22 16:42:30 -0700

    Error message is gone. Script is printing but the result is vague and unknown.

--- ScraperXBL.py
+++ ScraperXBL.py
@@ -1,24 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-
-# Setup Chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Ensure GUI is off
-
-# Set path to chromedriver as per your configuration
-webdriver_service = Service(ChromeDriverManager().install())
+# from webdriver_manager.chrome import ChromeDriverManager
 
 # Choose Chrome Browser
-driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+driver = webdriver.Chrome()
 
 # Open the website
 url = 'https://support.xbox.com/en-US/xbox-live-status'
 driver.get(url)
+
+# # Setup Chrome options
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")  # Ensure GUI is off
 
 # Wait for the page to load and the specific element to become available
 try:
@@ -29,9 +26,11 @@ try:
 
     # Find elements with the class 'undefined root-148'
     status_elements = driver.find_elements(By.CLASS_NAME, 'undefined')
-
+    print(status_elements)
     # Filter elements by additional class and print the text of the target element
     for element in status_elements:
+        print(element)
+        break
         classes = element.get_attribute('class')
         if 'root-148' in classes:
             print(element.text)
@@ -39,5 +38,5 @@ try:
 except Exception as e:
     print(f"An error occurred: {e}")
 
-# Close the browser
+# # Close the browser
 driver.quit()
